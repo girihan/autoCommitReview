63f0c4a8ea4f68f5705e4023b867c1497303c8cf
한상길(LGUPLUS) <girihan@lguplus.co.kr>
2025-03-19 13:40:12 +0900

    initial commit

---
+++ src/main/java/com/girihan/Main.java
@@ -0,0 +1,9 @@
+package com.girihan;
+
+public class Main {
+    public static void main(String[] args) {
+        Calculator calc = new Calculator();
+        int result = calc.add(1,2);
+        System.out.println("add: " + result);
+    }
+}
---
+++ src/test/java/com/girihan/CalculatorTest.java
@@ -0,0 +1,15 @@
+package com.girihan;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CalculatorTest {
+
+    @Test
+    void add() {
+        Calculator calculator = new Calculator();
+        Assertions.assertEquals(3,calculator.add(1,2));
+    }
+}
