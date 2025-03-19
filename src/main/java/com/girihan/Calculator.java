41ccb65f959fab6155455033b2cc86e7ddc09124
한상길(LGUPLUS) <girihan@lguplus.co.kr>
2025-03-19 15:21:41 +0900

    code changed

--- src/main/java/com/girihan/Calculator.java
+++ src/main/java/com/girihan/Calculator.java
@@ -2,6 +2,7 @@ package com.girihan;
 
 public class Calculator {
     public int add(int a, int b) {
+        System.out.println("add");
         return a + b;
     }
     public int subtract(int a, int b) {
